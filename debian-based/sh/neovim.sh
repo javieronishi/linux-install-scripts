85f8f8c3ceacb442124250657db25a010d7a9f57
Javier Onishi <javier-onishi@outlook.com>
2023-05-24 17:39:53 -0400

    ajustes en neovim

--- debian-based/sh/neovim.sh
+++ debian-based/sh/neovim.sh
@@ -1,9 +1,8 @@
+#!/bin/sh
 sleep 2
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-sudo chmod u+x nvim.appimage
+mkdir ~/App
+curl -Lo ~/App/nvim.appimage https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+sudo chmod u+x ~/App/nvim.appimage
+sudo ln -s ~/App/nvim.appimage /usr/bin/nvim:
 
-./nvim.appimage --appimage-extract
 
-# Optional: exposing nvim globally.
-sudo mv squashfs-root /
-sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
