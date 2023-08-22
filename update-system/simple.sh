bfb667d29400944033e0e3e7e1d5d4fc3e1c1d1c
Javier Onishi <javier-onishi@outlook.com>
2023-08-22 11:10:20 -0400

    refactor update system

---
+++ update-system/simple.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Ruta de la carpeta de logs en el directorio home
+LOGS_FOLDER="$HOME/.logs"
+
+# Crear la carpeta de logs si no existe
+mkdir -p "$LOGS_FOLDER"
+
+# Ruta del archivo de registro
+LOG_FECHA="log_fecha.log"
+
+PASSWORD_FILE="password.txt"
+
+# Leer el password de sudo desde el archivo
+sudo_password=$(cat "$PASSWORD_FILE")
+
+# Actualizar los paquetes del sistema
+echo "$sudo_password" | sudo -S apt update -y
+echo "$sudo_password" | sudo -S apt upgrade -y
+
+# Limpiar paquetes obsoletos
+echo "$sudo_password" | sudo -S apt autoremove -y
+echo "$sudo_password" | sudo -S apt autoclean -y
+
+# Actualizar el kernel del sistema
+echo "$sudo_password" | sudo -S apt dist-upgrade -y
+
+fecha_actual=$(date +'%Y-%m-%d %H:%M:%S')
+echo "Script ejecutado el: $fecha_actual" >> "$LOGS_FOLDER/$LOG_FECHA"
+
