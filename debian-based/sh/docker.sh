85855a8f58db29d9eaba13f9bb6b9d136fa87e5b
Javier Onishi <javier-onishi@outlook.com>
2024-02-09 10:25:40 -0400

    docker

--- debian-based/sh/docker.sh
+++ debian-based/sh/docker.sh
@@ -5,5 +5,6 @@ sudo sh get-docker.sh
 sudo usermod -aG docker $USER
 
 sleep 2s
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
