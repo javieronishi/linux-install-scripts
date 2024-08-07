cfc2ff4a150f3cf4c13e502266da9cc9d7515ab8
Javier Onishi <javier-onishi@outlook.com>
2024-08-07 10:38:56 -0400

    agrega readme para le update

---
+++ update-system/README.md
@@ -0,0 +1,9 @@
+Abre el archivo de configuración de cron:
+
+```
+crontab -e
+
+0 0 1 * * /home/javier/bash/update-system/update.sh
+```
+
+Guarda y cierra el archivo del crontab.
---
+++ update-system/update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Ruta del archivo de registro
+LOG_UPDATE="log_update.log"
+LOG_FECHA="log_fecha.log"
+LOG_PASSWORD_FILE="log_password_file.log"
+
+fecha_actual=$(date +'%Y-%m-%d %H:%M:%S')
+
+PASSWORD_FILE=".pwd.txt"
+
+# Verificar si el archivo de password existe
+if [ ! -f "$PASSWORD_FILE" ]; then
+  echo "El archivo de password $PASSWORD_FILE no existe: $fecha_actual" >> $LOG_PASSWORD_FILE
+  exit 1
+fi
+
+# Leer el password de sudo desde el archivo
+sudo_password=$(cat "$PASSWORD_FILE")
+
+# Actualizar los paquetes del sistema y guardar el detalle en el archivo de registro
+echo "$sudo_password" | sudo -S apt update -y | tee -a $LOG_UPDATE
+echo "$sudo_password" | sudo -S apt upgrade -y | tee -a $LOG_UPDATE
+
+# Limpiar paquetes obsoletos y guardar el detalle en el archivo de registro
+echo "$sudo_password" | sudo -S apt autoremove -y | tee -a $LOG_UPDATE
+echo "$sudo_password" | sudo -S apt autoclean -y | tee -a $LOG_UPDATE
+
+# Registrar la fecha y hora de ejecución del script
+echo "Script ejecutado el: $fecha_actual" >> $LOG_FECHA
