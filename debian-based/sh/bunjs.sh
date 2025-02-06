daf8388e2ad10c6fab12d0fc56e17b14c2ca08e5
Javier Onishi <javier-onishi@outlook.com>
2025-02-06 14:37:07 -0400

    bunjs

---
+++ debian-based/sh/bunjs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "=========================================="
+echo "=> Instalando Bun Js             ========="
+echo "=========================================="
+sleep 2s
+curl -fsSL https://bun.sh/install | bash
