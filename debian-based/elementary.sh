ae43d676ffb5325b77f57a37c205c27e92fe03b0
Javier Onishi <javier-onishi@outlook.com>
2025-04-11 09:47:05 -0400

    elementary

--- debian-based/elementary.sh
+++ debian-based/elementary.sh
@@ -1,40 +1,10 @@
 #!/bin/bash
 
-echo "ACTUALIZANDO EL SISTEMA"
-./sh/update.sh
+# INSTALA LO BÁSICO
+./basic.sh
 
-echo "INSTALANDO PANTHEON TWEAKS"
+# INSTALA PANTHEON TWEAKS
 ./sh/pantheon-tweaks.sh
 
-echo "INSTALANDO UBUNTU DRIVERS"
+# INSTALA UBUNTU DRIVERS
 ./sh/ubuntu-drivers.sh
-
-echo "INSTALANDO UTILIDADES COMUNES"
-./sh/utils.sh
-
-echo "ELIMINAR PAQUETES INNECESARIOS"
-./sh/autoclean.sh
-
-echo "INSTALANDO SPOTIFY"
-./sh/spotify.sh
-
-echo "INSTALANDO DOCKER"
-./sh/docker.sh
-
-echo "INSTALANDO SUBLIME TEXT"
-./sh/sublimetext.sh
-
-echo "INSTALANDO APP IMAGE LAUCHER"
-./sh/appimagelauncher.sh
-
-echo "INSTALANDO ULAUNCHER"
-./sh/ulauncher.sh
-
-echo "INSTALANDO VOLTA"
-./sh/volta.sh
-
-echo "INSTALANDO NEOVIM"
-./sh/neovim.sh
-
-echo "INSTALANDO .deb"
-./sh/software.sh
