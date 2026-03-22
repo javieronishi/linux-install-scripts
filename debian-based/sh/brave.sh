b4816ab18e44b8ea3e44930fe5078203dd172415
Javier Onishi <javier-onishi@outlook.com>
2026-03-21 23:28:47 -0400

    brave

--- debian-based/sh/brave.sh
+++ debian-based/sh/brave.sh
@@ -4,11 +4,4 @@ echo "=> Instalando Brave Browser      ========="
 echo "=========================================="
 sleep 2
 
-sudo apt install curl -y
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
-
-sudo apt update -y
-sudo apt install brave-browser -y
-
-# curl -fsS https://dl.brave.com/install.sh | sh
+curl -fsS https://dl.brave.com/install.sh | sh
